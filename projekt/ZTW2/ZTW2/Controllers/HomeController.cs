ea8a8bdbbb99ae164ccadf859dc94a059a38b747
gtef <203415@student.pwr.edu.pl>
2015-04-20 17:28:02 +0200

    MVC project
    moved previous project to Visual studio MVC 5 project

---
+++ projekt/ZTW2/ZTW2/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ZTW2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult milicz()
+        {
+            return View();
+        }
+
+        public ActionResult projekty()
+        {
+            return View();
+        }
+
+        public ActionResult zbyszek()
+        {
+            return View();
+        }
+
+        public ActionResult zespol()
+        {
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
